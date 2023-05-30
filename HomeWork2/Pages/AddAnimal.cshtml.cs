e009d75ffa951a47a5183bef6090dab9f06ad29d
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2023-05-30 11:46:59 +0300

    Add three pages Animals, AnimalDetails, AddAnimal

---
+++ HomeWork2/Pages/AddAnimal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HomeWork2.Pages
+{
+    public class AddAnimalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
