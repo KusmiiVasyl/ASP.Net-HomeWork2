51cb1c5ec2828ee8b279c39366e33adb1f478e3c
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2023-05-30 14:48:50 +0300

    Create show animals in Animals Page, style for navigation menu & footer

---
+++ HomeWork2/Models/Animal.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeWork2.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Sound { get; set; }
+    }
+}
---
+++ HomeWork2/Services/AnimalService.cs
@@ -0,0 +1,37 @@
+﻿using HomeWork2.Models;
+
+namespace HomeWork2.Services
+{
+    public class AnimalService
+    {
+        private static List<Animal> _animals = new List<Animal>()
+        {
+            new Animal()
+                {
+                    Id = 1,
+                    Name = "Dog",
+                    Sound = "Woof-woof"
+                },
+                new Animal()
+                {
+                    Id = 2,
+                    Name = "Cat",
+                    Sound = "Miaw-miaw"
+                },
+                new Animal()
+                {
+                    Id = 3,
+                    Name = "Cow",
+                    Sound = "My-y-y-y"
+                },
+                new Animal()
+                {
+                    Id = 4,
+                    Name = "Frog",
+                    Sound = "Kwa-kwa"
+                }
+        };
+
+        public static List<Animal> GetAnimals() => _animals;
+    }
+}
