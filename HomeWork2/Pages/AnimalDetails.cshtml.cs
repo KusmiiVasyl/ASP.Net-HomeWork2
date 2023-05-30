7bf63d9a1e9eec65f454a9a5f96cc85d2417d45b
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2023-05-30 22:03:36 +0300

    Create to show animal info on page AnimalDetails when click on Info on page Animals

--- HomeWork2/Pages/AnimalDetails.cshtml.cs
+++ HomeWork2/Pages/AnimalDetails.cshtml.cs
@@ -1,3 +1,5 @@
+using HomeWork2.Models;
+using HomeWork2.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,11 @@ namespace HomeWork2.Pages
 {
     public class AnimalDetailsModel : PageModel
     {
+        public Animal GetAnimal { get; set; }
         public void OnGet()
         {
+            int animalId = int.Parse(Request.Query["animalId"].ToString());
+            GetAnimal = AnimalService.GetAnimals().Single(a => a.Id == animalId);
         }
     }
 }
